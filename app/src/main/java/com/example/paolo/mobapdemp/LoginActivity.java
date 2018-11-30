385dacc333fb22f1d4d9e42395a276845919913b
Bowdown4488 <paolo_ato@dlsu.edu.ph>
2018-11-30 18:45:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/paolo/mobapdemp/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.paolo.mobapdemp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private EditText enterEmail;
+    private EditText enterPass;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){ //user is already loggedin
+            //put activity here
+        }
+        loginBtn = findViewById(R.id.loginBtn);
+        enterEmail = findViewById(R.id.enterEmail);
+        enterPass = findViewById(R.id.enterPass);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = enterEmail.getText().toString().trim();
+                String pass = enterPass.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(LoginActivity.this,"Please Enter Email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    Toast.makeText(LoginActivity.this,"Please Enter Password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email,pass)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(LoginActivity.this,"Logging in User: " + enterEmail.getText().toString().trim()
+                                            , Toast.LENGTH_LONG).show();
+                                    enterEmail.setText("");
+                                    enterPass.setText("");
+                                }else{
+                                    Toast.makeText(LoginActivity.this,"Could not login invalid username/password"
+                                            , Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/paolo/mobapdemp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.paolo.mobapdemp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private EditText registerEmail;
+    private EditText registerPass;
+    private FirebaseAuth firebaseAuth;
+    private TextView signinText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginBtn = findViewById(R.id.loginBtn);
+        registerEmail = findViewById(R.id.registerEmail);
+        registerPass = findViewById(R.id.registerPass);
+        signinText = findViewById(R.id.signinText);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = registerEmail.getText().toString().trim();
+                String pass = registerPass.getText().toString().trim();
+                //If no fields in these are entered
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(MainActivity.this,"Please Enter Email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    Toast.makeText(MainActivity.this,"Please Enter Password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                firebaseAuth.createUserWithEmailAndPassword(email,pass)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this,"User added to the database" , Toast.LENGTH_LONG).show();
+                                    registerEmail.setText("");
+                                    registerPass.setText("");
+                                }else{
+                                    Toast.makeText(MainActivity.this,"Could not register", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        signinText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        });
+
+    }
+}
